4079b146ca275e7df42c8fe975401f4bac71c908
Jordan Spell <jdspell@iu.edu>
2023-09-04 01:29:30 -0400

    updated test suite and got first working implementation completed

--- tests/test_similarity_metrics.py
+++ tests/test_similarity_metrics.py
@@ -9,8 +9,7 @@ from utils.similarity_metrics import dot_product
 
 
 def test_dot_product_calculates_correctly():
-    test_vector1 = np.ndarray([1, 2, 3])
-    test_vector2 = np.ndarray([4, 5, 6])
-    assert test_vector1.dot(test_vector2).array_equal(dot_product(test_vector1, test_vector2))
-    assert True
+    test_vector1 = np.array([1, 2, 3])
+    test_vector2 = np.array([4, 5, 6])
+    assert np.array_equal(test_vector1.dot(test_vector2), dot_product(test_vector1, test_vector2))
 
--- tests/test_testpy.py
+++
@@ -1,2 +0,0 @@
-def test_run_Test():
-    assert True
